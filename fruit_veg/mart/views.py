ac2a9dd7617116197aba1940e73e6f0a4f60f779
Saaqib.Chauhdry <saaqib.chauhdry@linoop.us>
2021-12-28 21:39:17 -0500

    edited requirements

--- fruit_veg/mart/views.py
+++ fruit_veg/mart/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
-import json
 from .forms import EmailForm
 
 #dummy data for sake of 'working with data for now'
